e8b4bc4a16fc4388393eba6af81ec82e27edc58c
luojh <luojh@biomarker.com.cn>
2018-01-24 17:11:22 +0800

    updata

--- demo/tabs/tabs.js
+++ demo/tabs/tabs.js
@@ -2,15 +2,48 @@
   var global = w;
   var once = 0;
   function Tab(config){
-    this.tag = config.tag
-    if(!config.tabList){
-      return console.log('no find tabList attr in config !')
+    this.config = config;
+    this.tag = config.tag;
+    this.currentId = 
+    if(this.config.tabList && this.config.navContainer){
+      this.navContainerEl = document.querySelector(this.config.navContainer);
+      this.createTabNav();
+      this.onClickNavButton()
+    }else{
+      return console.log('no find tabList attr in config ! ' + this.tag)
     }
-    this.createTabNav()
   }
   Tab.prototype = {
     constructor: Tab,
     createTabNav: function(){
+      var navTpl = '';
+      if(!this.navContainerEl){
+        return console.log('no find navContainer El !')
+      }
+      for(var navInx in this.config.tabList){
+        var nav = this.config.tabList[navInx];
+        navTpl += '<div class="'+this.config.tag+'_nav_item" id="'+this.config.tag+'_nav_'+navInx+'" title="'+nav.title+'">'+nav.label+'</div>';
+      }
+      this.navContainerEl.innerHTML = navTpl;
+    },
+    onClickNavButton: function(){
+      var self = this;
+      this.navContainerEl && this.navContainerEl.addEventListener('click', function(e){
+        var clickNavItemEl = e.path[0];
+        var currentClickId = clickNavItemEl.id.split('_')[2];
+        var currentCharacteristic = {
+          navTag: self.tag,
+          navItemIndex: currentClickId,
+          name: clickNavItemEl.innerText
+        };
+        var beforeStatue = self.config.beforeClickNav && self.config.beforeClickNav(currentCharacteristic)
+        if(beforeStatue){
+          this.changeCurrentNav()
+        }
+        debugger
+      })
+    },
+    changeCurrentNav: function(){
 
     }
   }
